c1ded1730846ac7e10b0d699c7449d92dae750dc
matheus <matheus.resteves@gmail.com>
2024-07-11 01:08:46 -0300

    First Commit

---
+++ src/Journey.Exception/ExceptionsBase/ErrorOnValidateException.cs
@@ -0,0 +1,23 @@
+﻿using System.Net;
+
+namespace Journey.Exception.ExceptionsBase
+{
+    public class ErrorOnValidateException : JourneyException
+    {
+        private readonly IList<string> _errors;
+        public ErrorOnValidateException(IList<string> messages) : base(string.Empty)
+        {
+            _errors = messages;
+        }
+
+        public override IList<string> GetErrorMessages()
+        {
+            return _errors;
+        }
+
+        public override HttpStatusCode GetStatusCode()
+        {
+            return HttpStatusCode.BadRequest;
+        }
+    }
+}
